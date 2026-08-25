9236be8abd73845ce65d917dc7159c172384cca1
haoyn231 <2054190139@qq.com>
2026-08-26 00:22:11 +0800

    chore: update blog slogan

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "好暖好温暖的博客",
-	subtitle: "Do one thing and do it well.",
+	subtitle: "靡不有初，鲜克有终",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
